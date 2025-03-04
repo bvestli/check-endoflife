ab06f0febcb7cb8ac74ca52478e945a795561477
Bjørn Vestli <bjorn.vestli@entur.org>
2025-03-04 10:38:24 +0100

    ci: add more tests

--- product/product_test.go
+++ product/product_test.go
@@ -1,8 +1,34 @@
 package product_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/bvestli/check_endoflife/product"
+)
 
 // TestGetSingleCycle tests the GetSingleCycle function
 func TestGetSingleCycle(t *testing.T) {
-  t.Skip("Not implemented yet")
+	singleCycleTests, err := product.GetSingleCycle("prometheus", "3.0")
+	if err != nil {
+		t.Errorf("GetSingleCycle(prometheus, 3.0) failed: %v", err)
+	}
+
+	if singleCycleTests.Name != "prometheus" {
+		t.Errorf("GetSingleCycle(prometheus, 3.0) failed: expected Name to be prometheus, got %s", singleCycleTests.Name)
+	}
+
+	if singleCycleTests.Latest != "3.0.1" {
+		t.Errorf("GetSingleCycle(prometheus, 3.0) failed: expected Latest to be 3.0.1, got %s", singleCycleTests.Cycle)
+	}
+
+	if singleCycleTests.ReleaseDate != "2024-11-14" {
+		t.Errorf("GetSingleCycle(prometheus, 3.0) failed: expected ReleaseDate to be 2024-11-14, got %s", singleCycleTests.ReleaseDate)
+	}
+}
+
+func TestGetLatestCycle(t *testing.T) {
+	_, err := product.GetLatestCycle("prometheus")
+	if err != nil {
+		t.Errorf("GetLatestCycle(prometheus) failed: %v", err)
+	}
 }
